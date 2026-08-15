4b71476ef6c1bf23ddc6fd556df5959ea6665128
DOTTO MTUNDU <dottomtundu9@gmail.com>
2026-08-16 01:56:48 +0300

    add pagnation

--- backend/app/routes/item.py
+++ backend/app/routes/item.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter,Depends
-from app.schemas.item import ItemUpdate,ItemCreate,ItemResponse
+from fastapi import APIRouter,Query,Depends
+from app.schemas.item import ItemUpdate,ItemCreate,ItemResponse,PaginationResponse
 from app.services import item
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.core.security import get_current_user
 from app.models.usermodel import User
+from typing import List
 
 router=APIRouter(prefix="/Item",tags=["Items"])
 
@@ -13,10 +14,16 @@ def add(data:ItemCreate,db:Session=Depends(get_db),
         current_user:User=Depends(get_current_user)):
     return item.create_item(data,db,current_user)
 
-@router.get("/",response_model=list[ItemResponse])
-def show_all(db:Session=Depends(get_db)):
-            
-    return item.get_all(db)
+@router.get("/",response_model=PaginationResponse)
+def show_all(
+    db:Session=Depends(get_db),location:str=None,status:str=None,
+    category:str=None,page:int=Query(1,ge=1),
+    limit:int=Query(10, ge=1, le=100),search:str=None,
+    item_type:str=None,sort:str=None,order:str="asc"
+     ):
+    return item.get_all(db,category,location,status,
+                        page,limit,search,
+                        item_type,sort,order)
 
 @router.get("/{item_id}",response_model=ItemResponse)
 def  show_single(item_id:int,db:Session=Depends(get_db)):
